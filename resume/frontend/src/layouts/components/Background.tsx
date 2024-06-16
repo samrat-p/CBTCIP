2758a4bc8ddf191da9ef0034a4cfa11cb392dff2
samrat-p <mitra.stark.68@gmail.com>
2024-06-16 17:40:35 +0530

    added custom gradient background

--- resume/frontend/src/layouts/components/Background.tsx
+++ resume/frontend/src/layouts/components/Background.tsx
@@ -1,7 +1,15 @@
-const Background = () => {
-    return (
-        
-            <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div> 
-}
+import { ReactNode } from "react";
 
-export default Background
+type BackgroundProps = {
+  children: ReactNode;
+};
+
+const Background = ({ children }: BackgroundProps) => {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 [background:radial-gradient(125%_125%_at_70%_30%,#000_35%,#63e_100%)]">
+      {children}
+    </div>
+  );
+};
+
+export default Background;
